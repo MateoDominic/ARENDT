b1dbb27eeb84162dcd1b3118a2c2e128624c9b90
zupson <mzupan4@algebra.hr>
2024-06-17 17:39:07 +0200

    1.implementacija login.js i register.js, 2.Promjena Id za pojedine elemente u HTML i konekcija sa JS, 3.preimenovanje foldera - nesmije sadrzavati zagrade u nazivu

---
+++ JS/login.js
@@ -0,0 +1,58 @@
+const btnClick = document.querySelector("#btn-login"); //promjeni id u html za btn
+const username = document.querySelector("#exampleInputUsername1");
+const password = document.querySelector("#exampleInputPassword1");
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("I am ready!");
+
+    btnClick.addEventListener("click", function (event) {
+        event.preventDefault(); //sprijecavamo defaultno slanje na server nego mi to radimo s JS
+
+        console.log("clicked");
+
+        const usernameValue = username.value;
+        const passwordValue = password.value;
+
+        if (!usernameValue || !passwordValue) {
+            alert("popuni podatke za prijavu");
+            return;
+        }
+
+        const loginData = {
+            username: usernameValue,
+            password: passwordValue,
+        };
+
+        console.log(usernameValue, passwordValue);
+
+        const response = fetch("http://localhost:5103/api/User/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        })
+        
+            .then(function (response) {
+                if (!response.ok) {
+                    throw new Error("Pogrešno korisnicko ime ili lozinka.");
+                }
+                return response.text();//mjenjano u .text iz .json()
+            })
+
+            .then(function (data) {
+                console.log("Token:", data.token);
+                localStorage.setItem("token",data.token);
+
+                
+                location.replace('../LoggedInUser/CreateKahootQuiz.html');
+
+               
+                console.log("Sucessiful login!");
+            })
+            .catch(function (error) {
+                console.error("Error: ", error);
+                console.log("FAILURE");
+            });
+    });
+});
